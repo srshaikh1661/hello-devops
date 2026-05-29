1af772d8c5389f2ee9261cc33674cadf157fd325
srshaikh1661 <sohailrs007@gmail.com>
2026-05-30 03:17:13 +0530

    Hello world changed to Hello sohail bhai

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ class MyHandler(SimpleHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(b"<h1>Hello World</h1>")
+        self.wfile.write(b"<h1>Hello Sohail Bhai</h1>")
 
 server = HTTPServer(('0.0.0.0', 8000), MyHandler)
 print("Server started on port 8000...")
